43170f17fbe93982145a6e45206aac57c1d65aca
Zheng Zheng <elancer.zh@gmail.com>
2017-11-08 14:38:21 -0500

    add a script to deploy vim dev env

---
+++ install_vim_plugin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# download .vimrc from my git repo and copy to home dir
+# git clone https://github.com/elancer-zh/ConfigBackup.git ~/
+cp ~/ConfigBackup/.vimrc ~/
+
+# install solarized theme
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+
+# install other plugin
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+
+
